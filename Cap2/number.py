a03d7a51df8cce2fb0c2b659249f0ee9ca9aed1c
Emily Lopez <emiily.jessica@hotmail.com>
2020-10-14 18:49:00 -0300

    exercicios manipulando numeros

---
+++ Cap2/number.py
@@ -0,0 +1,11 @@
+# Numeros com string - e necessario realizar a conversao
+age = 26
+message = "Happy " + str(age) + " rd Birthday!"
+print(message)
+
+# Inteiros
+print("Soma: 5 + 3 = " + str(5+3))
+print("Subtracao: 10 - 2 = " + str(10-2))
+print("Multiplicacao: 4 * 2 = " + str(4*2))
+print("Divisao: 16 / 2 = " + str(16/2))
+print("Exponencial: 2² = " + str(2**2))
