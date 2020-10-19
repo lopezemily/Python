47cc1857ed11e3e536b912079a1aa35dd3209c7a
Emily Lopez <emiily.jessica@hotmail.com>
2020-10-19 18:57:41 -0300

    conjecendo listas

---
+++ Cap3/listas.py
@@ -0,0 +1,16 @@
+nomes = ['emily', 'jessica', 'allan', 'lilian']
+print(nomes)
+
+# acessar elementos individuais de uma lista
+print(nomes[0])
+
+# concatenando strings com itens de uma lista
+message = "Meu segundo nome é " + nomes[1].title() + "."
+print(message)
+
+# acessar o ultimo item da lista
+print(nomes[-1])
+
+# alterar um elemento da lista
+nomes[2] = 'allan targino'
+print(nomes)
